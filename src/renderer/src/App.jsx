83a31e800d2b11b7bdba2bcc463e460d23431ace
Nhan Bui <buidev94@gmail.com>
2025-06-03 01:34:21 +0800

    feat: Add Mantine UI framework and Prettier Sort Import

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,34 +1,63 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { MantineProvider } from "@mantine/core";
+import { Tabs } from "@mantine/core";
+import {
+  IconMessageCircle,
+  IconPhoto,
+  IconSettings,
+} from "@tabler/icons-react";
 
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+  const ipcHandle = () => window.electron.ipcRenderer.send("ping");
 
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
+    <MantineProvider>
+      <Tabs defaultValue="gallery">
+        <Tabs.List>
+          <Tabs.Tab value="gallery" leftSection={<IconPhoto size={12} />}>
+            Gallery
+          </Tabs.Tab>
+          <Tabs.Tab
+            value="messages"
+            leftSection={<IconMessageCircle size={12} />}
+          >
+            Messages
+          </Tabs.Tab>
+          <Tabs.Tab value="settings" leftSection={<IconSettings size={12} />}>
+            Settings
+          </Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel value="gallery">Gallery tab content</Tabs.Panel>
+
+        <Tabs.Panel value="messages">Messages tab content</Tabs.Panel>
+
+        <Tabs.Panel value="settings">Settings tab content</Tabs.Panel>
+      </Tabs>
+      {/* <>
+        <img alt="logo" className="logo" src={electronLogo} />
+        <div className="creator">Powered by electron-vite</div>
+        <div className="text">
+          Build an Electron app with <span className="react">React</span>
         </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
+        <p className="tip">
+          Please try pressing <code>F12</code> to open the devTool
+        </p>
+        <div className="actions">
+          <div className="action">
+            <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
+              Documentation
+            </a>
+          </div>
+          <div className="action">
+            <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+              Send IPC
+            </a>
+          </div>
         </div>
-      </div>
-      <Versions></Versions>
-    </>
-  )
+        <Versions></Versions>
+      </> */}
+    </MantineProvider>
+  );
 }
 
-export default App
+export default App;
